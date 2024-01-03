23d19eae25c403a1f115cb3539491829e95f6e42
Bruno Pierri <pierri95@gmail.com>
2024-01-03 20:16:38 -0300

    initial project

---
+++ database/migrations/2024_01_03_230624_add_new_columns_to_despesas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('despesas', function (Blueprint $table) {
+            $table->double('valor_planejado', 8, 2);
+            $table->double('valor_real', 8, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('despesas', function (Blueprint $table) {
+            $table->double('valor_planejado', 8, 2);
+            $table->double('valor_real', 8, 2);
+        });
+    }
+};
